357b6a262313872013209340f1dd6bfdb46df6d1
Isaias Medina <isamed92@gmail.com>
2024-02-03 00:54:07 -0300

    Add OpenAI package and integrate it into GptService

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos/orthography.dto';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openIa = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
   // SOLO LLAMARA CASOS DE USO
 
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({ prompt: orthographyDto.prompt });
+    return await orthographyCheckUseCase(this.openIa, {
+      prompt: orthographyDto.prompt,
+    });
   }
 }
--- src/gpt/use-cases/orthography.usecase.ts
+++ src/gpt/use-cases/orthography.usecase.ts
@@ -1,11 +1,51 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openAi: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
-  return {
-    prompt: prompt,
-    apiKey: process.env.OPENAI_API_KEY,
-  };
+
+  const completion = await openAi.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `
+        Te serán proveídos textos con errores ortográficos y gramaticales,
+        Debes de responder en formato JSON, 
+        tu tarea es corregirlos y retornar información de las soluciones que has aplicado.
+        tambien debes de dar un porcentaje de acierto por el usuario, 
+
+        Si no hay errores, debes retornar un mensaje cordial de felicitaciones.
+
+        Ejemplo de salida:
+        {
+          userScore: number,
+          errors: string[], // ['La palabra #error# en realidad es #correction#']
+          message: string, // usa emojis y emoticonos para darle un toque de personalidad
+
+        }
+          `,
+      },
+
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+    max_tokens: 150,
+    temperature: 0.3,
+    // response_format: {
+    //   type: 'json_object',
+    // },
+  });
+
+  const data = JSON.parse(completion.choices[0].message.content);
+
+  return data;
 };
